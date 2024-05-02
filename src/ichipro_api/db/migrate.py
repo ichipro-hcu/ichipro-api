6adcbf399c12db94c6d58b015156b2da9045382b
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-05-02 20:23:56 +0000

    Feat: マイグレーション時にテストユーザを生成する

--- src/ichipro_api/db/migrate.py
+++ src/ichipro_api/db/migrate.py
@@ -19,6 +19,26 @@ print(
 # マイグレーション
 try:
     db.Base.metadata.create_all(__init__.engine)
+    testUser = db.User()
+    (
+        testUser.uniqueId,
+        testUser.email,
+        testUser.password,
+        testUser.faculty,
+        testUser.department,
+        testUser.grade,
+        testUser.classes,
+    ) = (
+        "testuser",
+        "test@sasakulab.com",
+        "5ac2ac5ba94dbce933c6719ca250bf1752d938f43367af6618ab9e9e30b57df701dcda95287c5af56d8374abf292813efa07e1f287b9e4877ff17969b6735fe1",  # p@ssw0rd
+        "学部情報",
+        "情報科学",
+        1,
+        "E",
+    )
+    session.add(testUser)
+    session.commit()
 except Exception as e:
     print("\033[31mMigration Error:\033[0m", e)
 else:
