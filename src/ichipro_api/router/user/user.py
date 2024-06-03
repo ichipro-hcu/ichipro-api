6a264996d43ccfce950819cfcffe7711893dad18
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-06-03 08:06:04 +0000

    Delete OAuth Self implementation

--- src/ichipro_api/router/user/user.py
+++ src/ichipro_api/router/user/user.py
@@ -1,5 +1,4 @@
 from fastapi import Depends, HTTPException, status, FastAPI, APIRouter
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 print("> router/user/user.py")
 
@@ -7,9 +6,6 @@ oa2Scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 app = FastAPI()
 router = APIRouter()
-
-# パスワードによる OAuth2 Credential について、以下のページを参照しています。
-# https://fastapi.tiangolo.com/ja/tutorial/security/simple-oauth2/
 
 fake = {
     "testuser": {
@@ -43,24 +39,3 @@ class User:
     iconUrl: str
     classes: str
 
-
-@router.post("/token")
-async def login(form_data: OAuth2PasswordRequestForm):
-    hitUserData = fake.get(form_data.username)
-    if not hitUserData:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = User(**hitUserData)
-    hashed_password = user.password
-    if not hashed_password == user.password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    return {"access_token": user.uniqueId, "token_type": "bearer"}
-
-
-@router.get("/me")
-async def read_users_me(token: str = Depends(oa2Scheme)):
-    result = {
-        "id": "01HX3HTE70BHNDTNXE9FSGKNQY",
-        "uniqueId": "testuser",
-        "expireTime": "2021-10-01T00:15:00",
-    }
-    return result
